7c54198720ac1833f9d37af4817755c3f38379eb
Billie Willie <vadimbelinovich@gmail.com>
2019-05-05 07:51:22 +0300

    edit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const budgetController = (function () {
+
+})();
+
+const uiController = (function () {
+
+})();
+
+const controller = (function (budgetctrl, uictrl) {
+
+})(budgetController, uiController);
