66e8642934aedb3e73a3f237ffecf11f97c2f1bc
Billie Willie <vadimbelinovich@gmail.com>
2019-05-05 22:41:18 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
-const budgetController = (function () {
+var budgetController = (function () {
 
 })();
 
-const uiController = (function () {
+var uiController = (function () {
+
+  var DOMstrings = {
+    inputType: document.querySelector('.add__type'),
+    inputDescription: document.querySelector('.add__description'),
+    inputValue: document.querySelector('.add__value'),
+    inputButton: document.querySelector('.add__btn'),
+  }
+
+  return {
+    getInput: function () {
+      return {
+        type: DOMstrings.inputType.value,
+        description: DOMstrings.inputDescription.value,
+        value: DOMstrings.inputValue.value,
+      }
+
+    }
+  }
 
 })();
 
-const controller = (function (budgetctrl, uictrl) {
+var controller = (function (budgetctrl, uictrl) {
+
+  function ctrlAddItem() {
+    var input = uictrl.getInput();
+    console.log(input);
+  }
+
+  DOMstrings.inputButton.addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13 || e.which === 13) {
+      ctrlAddItem();
+    }
+  })
 
 })(budgetController, uiController);
